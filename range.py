5b5a388183b3d07ec3865bc3cd236aa09d317568
kostotpe <a19910304@hotmail.com>
2023-03-25 15:32:28 +0800

    v1

---
+++ range.py
@@ -0,0 +1,22 @@
+# range 範圍
+# python 內建功能:可視為list產生器
+# range 常跟 for loop  搭配，讓內容重複執行'固定次數'
+import random
+
+range(5) #[0, 1, 2, 3, 4] #不含結尾值 如5
+range(3) #[0, 1, 2]
+
+for i in range(3):
+    print(3)
+    print('hi')
+for c in range(3):
+    r = random.randint(1,100)
+    print('這是第', c + 1, '次產生隨機數: ', r)
+
+# 延伸
+# range(起始值, 結束值)
+# range(起始值,結束值,遞增值)
+range(2, 5) # [2, 3, 4]
+range(2, 10, 3) # [2, 5, 8]
+range(3, 8, 2)
+range(10, 3, -2) # [10, 8, 6, 4]
